203da328b614dc21414af2c70a97220ed2a68d63
bharathi261 <bharathigowda11@gmail.com>
2025-11-21 14:59:25 +0530

    Update DbConfig.js

--- application-code/app-tier/DbConfig.js
+++ application-code/app-tier/DbConfig.js
@@ -1,5 +1,5 @@
 module.exports = Object.freeze({
-    DB_HOST : 'database-2.cluster-c1gy2yiw28ik.ap-northeast-2.rds.amazonaws.com',
+    DB_HOST : 'database-1.cluster-c1gy2yiw28ik.ap-northeast-2.rds.amazonaws.com',
     DB_USER : 'admin',
     DB_PWD : 'admin123',
     DB_DATABASE : 'webappdb'
